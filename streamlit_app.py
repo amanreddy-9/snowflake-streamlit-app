dfad24135a782563658ba720451ac93cce4bb06f
Aman Reddy Jukonti <amanreddyjukonti@gmail.com>
2026-03-20 16:17:21 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import requests
+import pandas as pd
 from snowflake.snowpark.functions import col
 
 # Write directly to the app
@@ -13,10 +14,11 @@ cnx = st.connection("snowflake")
 session = cnx.session()
 
 my_dataframe = session.table("smoothies.public.fruit_options").select(col("FRUIT_NAME"), col("SEARCH_ON"))
+pd_df = my_dataframe.to_pandas()
 
 ingredients_list = st.multiselect(
     "Choose up to 5 ingredients:",
-    my_dataframe,
+    pd_df["FRUIT_NAME"],
     max_selections=5
 )
 
@@ -27,8 +29,10 @@ if ingredients_list:
         ingredients_string += fruit_chosen + " "
         st.subheader(fruit_chosen + " Nutrition Information")
 
+        search_on = pd_df.loc[pd_df["FRUIT_NAME"] == fruit_chosen, "SEARCH_ON"].iloc[0]
+
         smoothiefroot_response = requests.get(
-            "https://my.smoothiefroot.com/api/fruit/" + fruit_chosen
+            "https://my.smoothiefroot.com/api/fruit/" + search_on
         )
 
         if smoothiefroot_response.status_code == 200:
