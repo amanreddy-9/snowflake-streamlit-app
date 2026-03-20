6fba793e61a9b1ae5a32b1a898f2fc59289fcdd0
Aman Reddy Jukonti <amanreddyjukonti@gmail.com>
2026-03-20 15:34:49 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,9 +2,8 @@ import streamlit as st
 from snowflake.snowpark.functions import col
 import requests
 
-# Write directly to the app
 st.title(":cup_with_straw: Customize Your Smoothie! :cup_with_straw:")
-st.write("""Choose the fruits you want in your custom Smoothie!""")
+st.write("Choose the fruits you want in your custom Smoothie!")
 
 name_on_order = st.text_input("Name on Smoothie:")
 st.write("The name on your Smoothie will be:", name_on_order)
@@ -25,8 +24,19 @@ if ingredients_list:
 
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + " "
-        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/" + fruit_chosen)
-        sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
+
+        search_on = str(fruit_chosen).lower().strip()
+        smoothiefroot_response = requests.get(
+            "https://my.smoothiefroot.com/api/fruit/" + search_on
+        )
+
+        if smoothiefroot_response.status_code == 200:
+            st.dataframe(
+                data=smoothiefroot_response.json(),
+                use_container_width=True
+            )
+        else:
+            st.error("Sorry, that fruit is not in our database.")
 
     time_to_insert = st.button("Submit Order")
 
